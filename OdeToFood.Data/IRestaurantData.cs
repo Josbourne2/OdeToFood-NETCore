918ec0fb0bd7c57d158f9050ab25b80ff8e2bb6c
Jos Menhart <Jos@estreme.eu>
2020-03-18 22:34:21 +0100

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,51 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id=1, Name="Jos's Resto", Location="Maryland", Cuisine=CuisineType.Italian},
+                new Restaurant{Id=2, Name="Jackrabbit Slim's",Location="Utrecht",Cuisine=CuisineType.Indian},
+                new Restaurant{Id=3, Name="Big Kahuna Burger", Location="Mexico", Cuisine=CuisineType.Mexican}
+            };
+
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            //return restaurants;
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r=>r.Id==id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name)||r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        [BindProperty(SupportsGet =true)]
+        public string SearchTerm { get; set; }
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; } //Waarom nog een getter en setter hier?
+
+        public ListModel(ILogger<IndexModel> logger, IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;            
+            this.restaurantData = restaurantData;
+            _logger = logger;
+        }
+
+        public void OnGet() //string searchTerm parameter komt overeen met naam van het search form
+        {
+            //HttpContext.Request.QueryString kan, maar er is een beter manier..
+            Message = config["Message"];
+            //Restaurants = restaurantData.GetAll();
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
+            _logger.LogInformation("Getting restaurants!");
+        }
+    }
+}
